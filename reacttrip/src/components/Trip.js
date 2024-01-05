f61fe3d34ae7ad0f909e3d20ac87c6a686492da5
ivenyao <ivenyao820109@gmail.com>
2024-01-04 18:49:20 -0800

    minor change

--- reacttrip/src/components/Trip.js
+++ reacttrip/src/components/Trip.js
@@ -4,8 +4,8 @@ import SearchBox from "./trip/search-box";
 import Plan from "./trip/plan-section";
 export default function Trip() {
     return (
-        <div className="flex h-auto flex-col md:flex-row md:overflow-hidden">
-            <div className="flex-grow md:overflow-y-auto">
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="flex-grow flex-col md:overflow-y-auto">
                 <Map/>
             </div>
             <div className="w-full flex-col flex md:w-96">
--- reacttrip/src/components/trip/map-section.js
+++ reacttrip/src/components/trip/map-section.js
@@ -8,7 +8,7 @@ export default function Map() {
     };
 
     return (
-        <div className='flex h-screen flex-col px-3 py-4 md:px-2'>
+        <div className='flex h-full flex-col px-3 py-4 md:px-2'>
             <div className="mb-2 h-full items-start justify-start rounded-md bg-blue-600 p-2">
                 MAP SECTION
                 {/* <GoogleMapReact
