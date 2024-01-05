a2323f45bc9b21052785f738a2555cc366444244
ivenyao <ivenyao820109@gmail.com>
2024-01-04 18:12:37 -0800

    init react, layout

---
+++ reacttrip/src/components/trip/search-box.js
@@ -0,0 +1,9 @@
+export default function SearchBox() {
+    return (
+        <div className='flex flex-col px-3 pt-4 md:px-2'>
+            <div className="flex h-full items-start justify-start rounded-md bg-blue-600 p-2">
+                Search Section
+            </div>
+        </div>
+    );
+}
