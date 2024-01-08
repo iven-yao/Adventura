a20ac068fbea81e6708fb4c66320d14202359126
ivenyao <ivenyao820109@gmail.com>
2024-01-07 19:29:08 -0800

    update map, plan and useState on Trip accross two child components

--- reacttrip/src/components/Trip.js
+++ reacttrip/src/components/Trip.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import MapSection from "./trip/map-section";
 import SearchBox from "./trip/search-box";
 import Plan from "./trip/plan-section";
+import { useJsApiLoader } from "@react-google-maps/api"; 
+
+const libraries = ['places'];
+const MAP_API_KEY = process.env.REACT_APP_GOOGLE_MAP_KEY || '';
+
 export default function Trip() {
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: MAP_API_KEY,
+        libraries: libraries
+    });
+
+    const [directionsResponse, setDirectionsResponse] = useState(null);
+
+    const setRoute = (newRoute) => {
+        setDirectionsResponse(newRoute);
+    }
+
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="flex-grow flex-col md:overflow-y-auto">
-                <MapSection/>
+                {isLoaded ? <MapSection route={directionsResponse}/> : <p>Map is Loading</p>}
             </div>
             <div className="w-full flex-col flex md:w-96">
                 <SearchBox/>
-                <Plan/>
+                <Plan setRoute={setRoute}/>
             </div>
         </div>
     );
--- reacttrip/src/components/trip/plan-section.js
+++ reacttrip/src/components/trip/plan-section.js
@@ -1,10 +1,86 @@
-export default function Plan() {
+import { useRef } from "react";
+import getRoute from "../mockapi/directionsServices";
+
+export default function Plan(props) {
+    const originRef = useRef();
+    const destiantionRef = useRef();
+    async function calculateRoute() {
+        if (originRef.current.value === "" || destiantionRef.current.value === "") {
+          return;
+        }
+        // const directionsService = new window.google.maps.DirectionsService();
+        // const results = await directionsService.route({
+        //   origin: originRef.current.value,
+        //   destination: destiantionRef.current.value,
+        //   travelMode: window.google.maps.TravelMode.DRIVING,
+        // });
+    
+        const results = getRoute();
+    
+        console.log(originRef.current.value);
+        console.log(destiantionRef.current.value);
+        console.log(results);
+    
+        
+    
+        props.setRoute(results);
+    }
+
+    function clearRoute() {
+        props.setRoute(null);
+        originRef.current.value = "";
+        destiantionRef.current.value = "";
+    }
+
     return (
         <div className='flex grow flex-col px-3 py-4 md:px-2'>
             <div className="mb-2 flex h-full items-start justify-start rounded-md bg-blue-600 p-2">
-                Plan Section<br/>
-                1....<br/>
-                2....<br/>
+                <div className="searchbox">
+                    <div className="row">
+                    <div className="col-lg-4">
+                        {/* <Autocomplete > */}
+                        <input
+                            type="text"
+                            name="Origin"
+                            className="form-control"
+                            placeholder="Origin"
+                            ref={originRef}
+                        />
+                        {/* </Autocomplete> */}
+                    </div>
+                    <div className="col-lg-4">
+                        {/* <Autocomplete> */}
+                        <input
+                            type="text"
+                            name="Destication"
+                            className="form-control"
+                            placeholder="Destication"
+                            ref={destiantionRef}
+                        />
+                        {/* </Autocomplete> */}
+                    </div>
+                    <div className="col-lg-2">
+                        <button
+                        type="submit"
+                        name="submit"
+                        className="btn btn-primary"
+                        onClick={calculateRoute}
+                        >
+                        Search
+                        </button>
+                    </div>
+                    <div className="col-lg-2">
+                        <button
+                        type="submit"
+                        name="clear"
+                        className="btn btn-danger"
+                        onClick={clearRoute}
+                        >
+                        Clear
+                        </button>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
